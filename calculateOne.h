eb5a16094aa627af16b96824202d9cb6a036b2eb
Steven McGraw <stemcg2613@students.ecpi.edu>
2025-12-10 12:33:08 -0500

    Create calculateOne.h

---
+++ calculateOne.h
@@ -0,0 +1,12 @@
+/***********************************************
+ * File: calculateOne.h
+ * Purpose: First calculation module
+ ***********************************************/
+#ifndef CALCULATEONE_H
+#define CALCULATEONE_H
+
+const char* calculationOne(void) {
+    return "Calculation One Result: 42";
+}
+
+#endif
