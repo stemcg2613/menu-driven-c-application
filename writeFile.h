b57a7145447d81f64d61385d9a03b9779fb0c86d
Steven McGraw <stemcg2613@students.ecpi.edu>
2025-12-10 12:32:23 -0500

    Create writeFile.h

---
+++ writeFile.h
@@ -0,0 +1,12 @@
+/***********************************************
+ * File: writeFile.h
+ * Purpose: Simulated write-to-file module
+ ***********************************************/
+#ifndef WRITEFILE_H
+#define WRITEFILE_H
+
+const char* writeFile(void) {
+    return "WriteFile Function Executed.";
+}
+
+#endif
