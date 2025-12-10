804d59a83c9d5481bd847eccc343867f4b5fdb6c
Steven McGraw <stemcg2613@students.ecpi.edu>
2025-12-10 12:32:41 -0500

    Create readFile.h

---
+++ readFile.h
@@ -0,0 +1,12 @@
+/***********************************************
+ * File: readFile.h
+ * Purpose: Simulated file-read module
+ ***********************************************/
+#ifndef READFILE_H
+#define READFILE_H
+
+const char* readFile(void) {
+    return "ReadFile Function Executed.";
+}
+
+#endif
