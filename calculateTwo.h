cb35be67c076a71e21838cfe2e67469da05d0569
Steven McGraw <stemcg2613@students.ecpi.edu>
2025-12-10 12:33:27 -0500

    Create calculateTwo.h

---
+++ calculateTwo.h
@@ -0,0 +1,12 @@
+/***********************************************
+ * File: calculateTwo.h
+ * Purpose: Second calculation module
+ ***********************************************/
+#ifndef CALCULATETWO_H
+#define CALCULATETWO_H
+
+const char* calculationTwo(void) {
+    return "Calculation Two Result: 84";
+}
+
+#endif
